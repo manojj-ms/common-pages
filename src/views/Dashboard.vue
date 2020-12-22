449a64371455ead15ff8d10ef959d0e41392d6cc
Manoj <Manoj Kumar>
2020-12-22 10:22:43 +0530

    Login,Register,Forgot,Dashboard

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+    </v-system-bar>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Dashboard</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      temporary
+    >
+      <v-toolbar flat>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-title class="title">
+            Home
+          </v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+    <v-toolbar flat>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-title class="title">
+            About
+          </v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+
+    <v-divider></v-divider>
+
+    <v-list dense class="pt-0">
+      <v-list-tile
+        v-for="item in items"
+        :key="item.title"
+        @click="items"
+      >
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main class="grey lighten-2">
+      <v-container>
+        <v-row>
+          <template v-for="n in 4">
+            <v-col
+              :key="n"
+              class="mt-2"
+              cols="12"
+            >
+              <strong>Category {{ n }}</strong>
+            </v-col>
+
+            <v-col
+              v-for="j in 6"
+              :key="`${n}${j}`"
+              cols="6"
+              md="2"
+            >
+              <v-sheet height="150"></v-sheet>
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
+</script>
---
+++ src/views/Register.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Register form</v-toolbar-title>
+                <v-spacer></v-spacer>
+                
+                
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field  name="name" label="Name" type="text"></v-text-field>
+                  <v-text-field  name="email" label="Email" type="text"></v-text-field>
+                  <v-text-field id="password"  name="password" label="Password" type="password"></v-text-field>
+                  <v-text-field id="confirmpassword"  name="cpassword" label="ConfirmPassword" type="password"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Register</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
